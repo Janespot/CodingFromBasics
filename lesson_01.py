3b056368d87a6018fd2bd29e0b714857ef5b20df
Jane Odhiambo <58133653+Janespot@users.noreply.github.com>
2026-09-10 21:24:24 -0400

    binary to decimal converter

--- lesson_01.py
+++ lesson_01.py
@@ -1,5 +1,36 @@
+#Intro to binary and decimal conversions
 decimal = 11
 print("decimal: ", decimal)
 
 binary = 1011
 print("binary: ", binary)
+
+# Binary to decimal converter
+binary = "1011"
+
+decimal = (
+    int(binary[0]) * 8 +
+    int(binary[1]) * 4 +
+    int(binary[2]) * 2 +
+    int(binary[3]) * 1
+)
+
+print("Binary: ", binary, ", Decimal: ", decimal)
+
+
+# Binary to decimal converter for any length binary number
+binary = "11011000"
+
+b_length = len(binary) - 1
+
+print(b_length)
+
+decimal = 0
+
+for b in binary:
+
+    power = 2 ** b_length
+    decimal += int(b) * power
+    b_length -= 1
+
+print("Binary: ", binary, ", Decimal: ", decimal)
